d33df76ecf39ad5a20162712e167eb4819b18fd2
Andrey <pinchuk.andryusha310212@gmail.com>
2023-02-02 13:51:54 +0200

    feat: add swagger-ui

--- src/main/java/com/helloworld/hello_world/controller/PhotoController.java
+++ src/main/java/com/helloworld/hello_world/controller/PhotoController.java
@@ -1,9 +1,11 @@
 package com.helloworld.hello_world.controller;
 
 import com.helloworld.hello_world.controller.dto.PhotoDto;
-import com.helloworld.hello_world.controller.dto.PhotoMapper;
+import com.helloworld.hello_world.controller.dto.mapper.PhotoMapper;
 import com.helloworld.hello_world.repository.entity.Photo;
 import com.helloworld.hello_world.service.PhotoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ public class PhotoController {
     private final PhotoService photoService;
     private final PhotoMapper photoMapper;
 
+    @Operation(summary = "Get all photos")
     @GetMapping
     public ResponseEntity<List<PhotoDto>> findAll() {
         List<PhotoDto> responseDto = photoMapper.toDto(photoService.findAll());
@@ -26,6 +29,7 @@ public class PhotoController {
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "Get photo by id")
     @GetMapping("/{id}")
     public ResponseEntity<PhotoDto> findById(@PathVariable Long id) {
         PhotoDto responseDto = photoMapper.toDto(photoService.findById(id));
@@ -33,20 +37,25 @@ public class PhotoController {
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "Find photo by description")
     @GetMapping("/description")
+    @Parameter(name = "description", description = "Description of photo")
     public ResponseEntity<PhotoDto> findByDescription(@RequestParam String description) {
         PhotoDto responseDto = photoMapper.toDto(photoService.findByDescription(description));
 
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "Find photos by description containing specified text")
     @GetMapping("/containing")
+    @Parameter(name = "description", description = "Text to search in descriptions")
     public ResponseEntity<List<PhotoDto>> findByDescriptionContaining(@RequestParam String description) {
         List<PhotoDto> responseDto = photoMapper.toDto(photoService.findByDescriptionContaining(description));
 
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "Save photo")
     @PostMapping
     public ResponseEntity<PhotoDto> savePhoto(@RequestBody PhotoDto photoDto) {
         Photo response = photoService.savePhoto(photoMapper.toDomain(photoDto));
@@ -55,6 +64,7 @@ public class PhotoController {
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update photo")
     @PutMapping
     public ResponseEntity<PhotoDto> updatePhoto(@RequestBody PhotoDto photoDto) {
         Photo response = photoService.updatePhoto(photoMapper.toDomain(photoDto));
@@ -63,6 +73,7 @@ public class PhotoController {
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "Delete photo by id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePhoto(@PathVariable Long id) {
         photoService.deletePhoto(id);
