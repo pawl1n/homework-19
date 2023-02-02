a5163a8f3914f01cfa43a9045fb1801a13e4222f
Andrey <pinchuk.andryusha310212@gmail.com>
2023-02-02 13:52:25 +0200

    refactor: change DTOs' type to record

--- src/main/java/com/helloworld/hello_world/controller/dto/PhotoDto.java
+++ src/main/java/com/helloworld/hello_world/controller/dto/PhotoDto.java
@@ -1,11 +1,9 @@
 package com.helloworld.hello_world.controller.dto;
 
-import lombok.Data;
-
-@Data
-public class PhotoDto {
-    private Long id;
-    private String url;
-    private String description;
-    private Long studentId;
+public record PhotoDto(
+        Long id,
+        String url,
+        String description,
+        Long studentId
+) {
 }
